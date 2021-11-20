2db2336cf3b0355aa4d8537cf74b1223aa9def31
Kurtis <gears2525@aol.com>
2021-11-19 21:06:20 -0800

    setup the questions

--- index.js
+++ index.js
@@ -1,40 +1,122 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const Choices = require('inquirer/lib/objects/choices');
-const questions = () => {
-    inquirer
-        .prompt(
-            [   {
+const generateMarkdown = require ('./utils/generateMarkdown')
+
+const promptUser = () => {
+    return inquirer.prompt(
+            [{
                     type: 'input',
-                    name: 'name',
-                    message: 'What is your name?'
+                    name: 'title',
+                    message: 'Project title:',
+                    validate: titleInput => {
+                        if (titleInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a project title!');
+                            return false;
+                        };
+                    }
                 },
                 {
-                    type: 'checkbox',
-                    name: 'communication',
-                    message: 'What is you preferred method of communication?',
-                    choices: ['email', 'phone', 'slack', 'mail']
+                    type: 'input',
+                    name: 'description',
+                    message: 'Provide a description of the project (Required)',
+                    validate: descriptionInput => {
+                        if (descriptionInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a project description!');
+                            return false;
+                        };
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'instructions',
+                    message: 'Provide installation instructions for the project (Required)',
+                    validate: instructionsInput => {
+                        if (instructionsInput) {
+                            return true;
+                        } else {
+                            console.log('Please add installation instructions!');
+                            return false;
+                        };
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'usage',
+                    message: 'Provide usage information for the project (Required)',
+                    validate: usageInput => {
+                        if (usageInput) {
+                            return true;
+                        } else {
+                            console.log('Please provide usage information!');
+                            return false;
+                        };
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'usage',
+                    message: 'Provide usage information for the project (Required)',
+                    validate: usageInput => {
+                        if (usageInput) {
+                            return true;
+                        } else {
+                            console.log('Please provide usage information!');
+                            return false;
+                        };
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'contribution ',
+                    message: 'Provide contribution guidelines for the project (Required)',
+                    validate: contributionInput => {
+                        if (contributionInput) {
+                            return true;
+                        } else {
+                            console.log('Please provide usage information!');
+                            return false;
+                        };
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'test ',
+                    message: 'Provide test instructions for the project (Required)',
+                    validate: testInput => {
+                        if (testInput) {
+                            return true;
+                        } else {
+                            console.log('Please provide test instructions!');
+                            return false;
+                        };
+                    }
                 },
                 {
                     type: 'list',
-                    name: 'language',
-                    message: 'What language do you know?',
-                    choices: ['English', 'Spanish', 'French', 'others']
-                }
-            ])
-        .then((answers) => {
-            writeToFile(answers);
+                    name: 'license',
+                    message: 'Please select a license.',
+                    choices: ['None', 'MIT License', 'GNU GPL v3', 'Apache 2.0 License']
+                },
+            ]).then((data) => {
+            questions = {data};
+            generateMarkdown(questions);
+            // writeToFile(questions);
         })
         .catch((error) => {
             console.log(error);
         });
+
 };
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
-const writeToFile = data => {
-    fs.writeFile('README.md', JSON.stringify(data, null, '\t'), function (err) {
+const writeToFile = (data) => {
+     fs.writeFile('./dist/README.md', JSON.stringify(data, null, '\t'), function (err) {
         if (err) {
             return console.log(err);
         }
@@ -42,10 +124,11 @@ const writeToFile = data => {
     });
 };
 
+
 // TODO: Create a function to initialize app
 function init() {
-    questions();
-}
+     promptUser();
+};
 
 // Function call to initialize app
-init();
+init()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT License":
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case "GNU GPL v3":
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case "Apache 2.0 License":
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    default:
+      return ' ';
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +23,9 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data);
 
-`;
+  // return `# ${data.title}`;
 }
 
 module.exports = generateMarkdown;
