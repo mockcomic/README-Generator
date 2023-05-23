b082301dfb3867c8332fe6b109d5da577f87f20f
Kurtis Hight <gears2525@aol.com>
2023-05-23 12:31:22 -0700

    fixed mission dist

--- index.js
+++ index.js
@@ -1,144 +1,144 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown');
 
-const promptUser =  () => {
-    inquirer.prompt(
-        [{
-            type: 'input',
-            name: 'title',
-            message: 'Project title:',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a project title!');
-                    return false;
-                };
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your GitHub username? (Required)',
-            validate: testInput => {
-                if (testInput) {
-                    return true;
-                } else {
-                    console.log('Please enter you GitHub username!');
-                    return false;
-                };
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email? (Required)',
-            validate: testInput => {
-                if (testInput) {
-                    return true;
-                } else {
-                    console.log('Please enter you email!');
-                    return false;
-                };
-            }
-        },
-        {
-                    type: 'input',
-                    name: 'description',
-                    message: 'Provide a description of the project (Required)',
-                    validate: descriptionInput => {
-                        if (descriptionInput) {
-                            return true;
-                        } else {
-                            console.log('Please enter a project description!');
-                            return false;
-                        };
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'instructions',
-                    message: 'Provide installation instructions for the project (Required)',
-                    validate: instructionsInput => {
-                        if (instructionsInput) {
-                            return true;
-                        } else {
-                            console.log('Please add installation instructions!');
-                            return false;
-                        };
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'usage',
-                    message: 'Provide usage information for the project (Required)',
-                    validate: usageInput => {
-                        if (usageInput) {
-                            return true;
-                        } else {
-                            console.log('Please provide usage information!');
-                            return false;
-                        };
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'contribution',
-                    message: 'Provide contribution guidelines for the project (Required)',
-                    validate: contributionInput => {
-                        if (contributionInput) {
-                            return true;
-                        } else {
-                            console.log('Please provide usage information!');
-                            return false;
-                        };
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'test',
-                    message: 'Provide test instructions for the project (Required)',
-                    validate: testInput => {
-                        if (testInput) {
-                            return true;
-                        } else {
-                            console.log('Please provide test instructions!');
-                            return false;
-                        };
-                    }
-                },
-                {
-                    type: 'list',
-                    name: 'license',
-                    message: 'Please select a license.',
-                    choices: ['None', 'MIT License', 'GNU GPL v3', 'Apache 2.0 License']
-                },
-            ])
-        .then(data => generateMarkdown(data))
-        .then(stringData => writeToFile(stringData))
-        .catch((error) => {
-            console.log(error);
-        });
+const promptUser = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'title',
+				message: 'Project title:',
+				validate: titleInput => {
+					if (titleInput) {
+						return true;
+					} else {
+						console.log('Please enter a project title!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'github',
+				message: 'What is your GitHub username? (Required)',
+				validate: testInput => {
+					if (testInput) {
+						return true;
+					} else {
+						console.log('Please enter you GitHub username!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'email',
+				message: 'What is your email? (Required)',
+				validate: testInput => {
+					if (testInput) {
+						return true;
+					} else {
+						console.log('Please enter you email!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'description',
+				message: 'Provide a description of the project (Required)',
+				validate: descriptionInput => {
+					if (descriptionInput) {
+						return true;
+					} else {
+						console.log('Please enter a project description!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'instructions',
+				message: 'Provide installation instructions for the project (Required)',
+				validate: instructionsInput => {
+					if (instructionsInput) {
+						return true;
+					} else {
+						console.log('Please add installation instructions!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'usage',
+				message: 'Provide usage information for the project (Required)',
+				validate: usageInput => {
+					if (usageInput) {
+						return true;
+					} else {
+						console.log('Please provide usage information!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'contribution',
+				message: 'Provide contribution guidelines for the project (Required)',
+				validate: contributionInput => {
+					if (contributionInput) {
+						return true;
+					} else {
+						console.log('Please provide usage information!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'input',
+				name: 'test',
+				message: 'Provide test instructions for the project (Required)',
+				validate: testInput => {
+					if (testInput) {
+						return true;
+					} else {
+						console.log('Please provide test instructions!');
+						return false;
+					}
+				},
+			},
+			{
+				type: 'list',
+				name: 'license',
+				message: 'Please select a license.',
+				choices: ['None', 'MIT License', 'GNU GPL v3', 'Apache 2.0 License'],
+			},
+		])
+		.then(data => generateMarkdown(data))
+		.then(stringData => writeToFile(stringData))
+		.catch(error => {
+			console.log(error);
+		});
 };
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
-const writeToFile = (data) => {
-    fs.writeFile('./dist/README.md', data, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Success!");
-    });
+const writeToFile = data => {
+	fs.writeFile('./dist/README.md', data, function (err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log('Success!');
+	});
 };
-
 
 // TODO: Create a function to initialize app
 function init() {
-    promptUser();
-};
+	promptUser();
+}
 
 // Function call to initialize app
-init()
+init();
